0cd2c0bde220406252926e1e8f6eca5762539dfd
GreenFrog-fs <aaaartemaa@gmail.com>
2024-09-12 13:49:29 +0700

    desctop git

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ bootstrap();
 import { Bot, InlineKeyboard } from 'grammy';
 const url = 'https://5e77-85-143-79-243.ngrok-free.app';
 const bot = new Bot('7530315714:AAHXK60ZgFsMOw01X2jQJ0IfOk7SwIIQcNM');
+
 console.log('бот запускается');
 bot.command('start', (ctx) => {
   ctx.reply('Добро пожаловать в Maps&Chests', {
