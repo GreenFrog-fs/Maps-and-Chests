a48427c9222f2c14bb676dd020658921bf986486
GreenFrog-fs <aaaartemaa@gmail.com>
2024-09-12 14:58:31 +0700

    logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('/chests')
   getChests(@Query('lat') lat: string, @Query('lon') lon: string) {
+    console.log("запрос выдачи сундуков")
     return this.appService.getChests(+lat, +lon);
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,15 +3,17 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get(':id')
   getUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log("запрос поиска")
     return this.userService.findUserById(id);
   }
 
   @Post(':id')
   createUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log("запрос добавления")
     return this.userService.createUserById(id);
   }
 }
